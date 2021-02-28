03b4b9ddda0827d0450807553324712e1090c0b3
justincodex <79738215+justincodex@users.noreply.github.com>
2021-02-28 07:06:21 +0530

    High car speed is added

--- Car.java
+++ Car.java
@@ -1,7 +1,9 @@
 class Car{
   public static void main(String []args){
     
-    int Avg_car_speed =100;
+    int Avg_car_speed = 100;
+    
+    int high_car_speed =350
     
     System.out.println("Car class");
   }
